8451f3afe709561bbd0f7c9f27ad08e7833ced86
Zofia Nora <sophienora92@gmail.com>
2025-06-03 19:17:47 +0200

    added content for the svg icons

---
+++ whateverApp/src/components/LessonIcons.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const LockIcon = () => (
+  <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 9V6C5 4.67392 5.52678 3.40215 6.46447 2.46447C7.40215 1.52678 8.67392 1 10 1C11.3261 1 12.5979 1.52678 13.5355 2.46447C14.4732 3.40215 15 4.67392 15 6V9M11 15C11 15.5523 10.5523 16 10 16C9.44772 16 9 15.5523 9 15C9 14.4477 9.44772 14 10 14C10.5523 14 11 14.4477 11 15ZM3 9H17C18.1046 9 19 9.89543 19 11V19C19 20.1046 18.1046 21 17 21H3C1.89543 21 1 20.1046 1 19V11C1 9.89543 1.89543 9 3 9Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
+
+export const BookIcon = () => (
+  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M11 1L14.09 7.26L21 8.27L16 13.14L17.18 20.02L11 16.77L4.82 20.02L6 13.14L1 8.27L7.91 7.26L11 1Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
+
+export const CheckIcon = () => (
+  <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M17 1L6 12L1 7" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
