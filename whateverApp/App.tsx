8203ff786fb074f6ee61b595d2c1d8c179cc70a4
Leon <27236884+Leonpa@users.noreply.github.com>
2025-06-03 20:24:29 +0200

    refactor

--- whateverApp/App.tsx
+++ whateverApp/App.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import {LogBox} from 'react-native';
-import Main from './src/navigation/Main.tsx';
-
-LogBox.ignoreLogs(['new NativeEventEmitter']); // optional
+import {StatusBar, useColorScheme, View, StyleSheet} from 'react-native';
+import {NavigationContainer, useNavigationContainerRef} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation/AppNavigator';
+import {CustomHeader} from './src/components/CustomHeader';
 
 export default function App() {
-  return <Main />;
+  const isDarkMode = useColorScheme() === 'dark';
+  const navigationRef = useNavigationContainerRef();
+  const [currentRoute, setCurrentRoute] = React.useState<string>();
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <CustomHeader currentRoute={currentRoute} navigationRef={navigationRef} />
+      <NavigationContainer
+        ref={navigationRef}
+        onReady={() => {
+          setCurrentRoute(navigationRef.getCurrentRoute()?.name);
+        }}
+        onStateChange={() => {
+          setCurrentRoute(navigationRef.getCurrentRoute()?.name);
+        }}
+      >
+        <SafeAreaView style={styles.navigator}>
+          <AppNavigator />
+        </SafeAreaView>
+      </NavigationContainer>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000', // Adjust as needed or use your Colors file
+  },
+  navigator: {
+    flex: 1,
+  },
+});
